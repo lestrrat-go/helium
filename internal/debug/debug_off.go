17756a52eed692cfce3cc1cd8fa9e3dd572ffdfe
Daisuke Maki <lestrrat+github@gmail.com>
2015-12-24 14:15:59 +0900

    allow compilation when debug is off

--- internal/debug/debug_off.go
+++ internal/debug/debug_off.go
@@ -4,6 +4,12 @@ package debug
 
 const Enabled = false
 
+type guard func()
+func (g guard) IRelease(f string, args ...interface{}) {}
+
+// IPrintf is no op unless you comple with the `debug` tag
+func IPrintf(f string, args ...interface{}) guard { return nil }
+
 // Printf is no op unless you compile with the `debug` tag
 func Printf(f string, args ...interface{}) {}
 
