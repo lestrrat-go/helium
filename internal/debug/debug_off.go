1486e5c8c496919b07da5dac970311ee74ba536b
Daisuke Maki <lestrrat+github@gmail.com>
2015-12-25 13:16:53 +0900

    appease the compiler

--- internal/debug/debug_off.go
+++ internal/debug/debug_off.go
@@ -8,7 +8,7 @@ type guard struct{}
 func (g guard) IRelease(f string, args ...interface{}) {}
 
 // IPrintf is no op unless you comple with the `debug` tag
-func IPrintf(f string, args ...interface{}) guard { return nil }
+func IPrintf(f string, args ...interface{}) guard { return guard{} }
 
 // Printf is no op unless you compile with the `debug` tag
 func Printf(f string, args ...interface{}) {}
