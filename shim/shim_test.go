e3203ebcd0460160eba50eb0d368333fd81821ef
Daisuke Maki <lestrrat+github@gmail.com>
2026-03-03 12:52:23 +0900

    add phase 1 shim tests

--- shim/shim_test.go
+++ shim/shim_test.go
@@ -3,6 +3,7 @@ package shim_test
 import (
 	"bytes"
 	stdxml "encoding/xml"
+	"fmt"
 	"io"
 	"testing"
 
@@ -118,4 +119,163 @@ func TestAPICompilesWithInterfaces(t *testing.T) {
 	var _ io.Reader = bytes.NewReader(nil)
 	_ = shim.NewDecoder(bytes.NewReader(nil))
 	_ = shim.NewEncoder(io.Discard)
+}
+
+func TestRawTokenSequenceMatchesStdlib(t *testing.T) {
+	input := []byte(`<root><a x="1">t</a><!--c--><?pi d?></root>`)
+
+	stdDec := stdxml.NewDecoder(bytes.NewReader(input))
+	shimDec := shim.NewDecoder(bytes.NewReader(input))
+
+	var stdSeq []string
+	for {
+		tok, err := stdDec.RawToken()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		stdSeq = append(stdSeq, tokenRepr(tok))
+	}
+
+	var shimSeq []string
+	for {
+		tok, err := shimDec.RawToken()
+		if err == io.EOF {
+			break
+		}
+		require.NoError(t, err)
+		shimSeq = append(shimSeq, tokenRepr(tok))
+	}
+
+	require.Equal(t, stdSeq, shimSeq, "RawToken sequence mismatch")
+}
+
+func TestSkipBehaviorMatchesStdlib(t *testing.T) {
+	input := []byte(`<root><skip><a/><b>txt</b></skip><keep/></root>`)
+
+	stdDec := stdxml.NewDecoder(bytes.NewReader(input))
+	shimDec := shim.NewDecoder(bytes.NewReader(input))
+
+	consumeUntilStart := func(next func() (stdxml.Token, error), name string) {
+		for {
+			tok, err := next()
+			require.NoError(t, err)
+			se, ok := tok.(stdxml.StartElement)
+			if ok && se.Name.Local == name {
+				return
+			}
+		}
+	}
+
+	consumeUntilStart(stdDec.Token, "skip")
+	consumeUntilStart(func() (stdxml.Token, error) { return shimDec.Token() }, "skip")
+
+	require.NoError(t, stdDec.Skip())
+	require.NoError(t, shimDec.Skip())
+
+	var stdNext, shimNext string
+	for {
+		tok, err := stdDec.Token()
+		require.NoError(t, err)
+		if se, ok := tok.(stdxml.StartElement); ok {
+			stdNext = se.Name.Local
+			break
+		}
+	}
+	for {
+		tok, err := shimDec.Token()
+		require.NoError(t, err)
+		if se, ok := tok.(shim.StartElement); ok {
+			shimNext = se.Name.Local
+			break
+		}
+	}
+
+	require.Equal(t, "keep", stdNext)
+	require.Equal(t, stdNext, shimNext)
+}
+
+func TestInputOffsetAndPosMatchStdlib(t *testing.T) {
+	input := []byte("<root>abc</root>")
+	stdDec := stdxml.NewDecoder(bytes.NewReader(input))
+	shimDec := shim.NewDecoder(bytes.NewReader(input))
+
+	for i := 0; i < 2; i++ {
+		_, err := stdDec.Token()
+		require.NoError(t, err)
+		_, err = shimDec.Token()
+		require.NoError(t, err)
+	}
+
+	require.Equal(t, stdDec.InputOffset(), shimDec.InputOffset(), "InputOffset mismatch")
+
+	stdLine, stdCol := stdDec.InputPos()
+	shimLine, shimCol := shimDec.InputPos()
+	require.Equal(t, stdLine, shimLine, "InputPos line mismatch")
+	require.Equal(t, stdCol, shimCol, "InputPos col mismatch")
+}
+
+func TestEncoderEncodeAndEncodeElementMatchStdlib(t *testing.T) {
+	type item struct {
+		XMLName stdxml.Name `xml:"item"`
+		Value   string      `xml:",chardata"`
+	}
+
+	val := item{Value: "hello"}
+
+	var stdBuf bytes.Buffer
+	stdEnc := stdxml.NewEncoder(&stdBuf)
+	require.NoError(t, stdEnc.Encode(val))
+	require.NoError(t, stdEnc.EncodeElement(item{Value: "world"}, stdxml.StartElement{Name: stdxml.Name{Local: "x"}}))
+	require.NoError(t, stdEnc.Flush())
+
+	var shimBuf bytes.Buffer
+	shimEnc := shim.NewEncoder(&shimBuf)
+	require.NoError(t, shimEnc.Encode(val))
+	require.NoError(t, shimEnc.EncodeElement(item{Value: "world"}, stdxml.StartElement{Name: stdxml.Name{Local: "x"}}))
+	require.NoError(t, shimEnc.Flush())
+
+	require.Equal(t, stdBuf.Bytes(), shimBuf.Bytes(), "Encode/EncodeElement output mismatch")
+}
+
+func TestEncoderIndentMatchesStdlib(t *testing.T) {
+	type root struct {
+		XMLName stdxml.Name `xml:"root"`
+		Child   string      `xml:"child"`
+	}
+
+	v := root{Child: "v"}
+
+	var stdBuf bytes.Buffer
+	stdEnc := stdxml.NewEncoder(&stdBuf)
+	stdEnc.Indent("", "  ")
+	require.NoError(t, stdEnc.Encode(v))
+	require.NoError(t, stdEnc.Flush())
+
+	var shimBuf bytes.Buffer
+	shimEnc := shim.NewEncoder(&shimBuf)
+	shimEnc.Indent("", "  ")
+	require.NoError(t, shimEnc.Encode(v))
+	require.NoError(t, shimEnc.Flush())
+
+	require.Equal(t, stdBuf.Bytes(), shimBuf.Bytes(), "Indent output mismatch")
+}
+
+func tokenRepr(tok stdxml.Token) string {
+	switch v := tok.(type) {
+	case stdxml.StartElement:
+		return fmt.Sprintf("S:%s attrs=%v", v.Name.Local, v.Attr)
+	case stdxml.EndElement:
+		return fmt.Sprintf("E:%s", v.Name.Local)
+	case stdxml.CharData:
+		return fmt.Sprintf("C:%s", string(v))
+	case stdxml.Comment:
+		return fmt.Sprintf("M:%s", string(v))
+	case stdxml.ProcInst:
+		return fmt.Sprintf("P:%s:%s", v.Target, string(v.Inst))
+	case stdxml.Directive:
+		return fmt.Sprintf("D:%s", string(v))
+	default:
+		return fmt.Sprintf("%T", tok)
+	}
 }
