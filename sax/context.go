5f831408ce278be64ccd2f21c170f66cc7fc1635
Daisuke Maki <lestrrat+github@gmail.com>
2026-03-09 10:55:15 +0900

    add nil context guards to DocumentLocator helpers

--- sax/context.go
+++ sax/context.go
@@ -10,11 +10,17 @@ const (
 
 // SetDocumentLocatorValue stores a DocumentLocator in the context, returning a new context.
 func SetDocumentLocatorValue(ctx context.Context, loc DocumentLocator) context.Context {
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	return context.WithValue(ctx, documentLocatorKey, loc)
 }
 
 // GetDocumentLocator retrieves the DocumentLocator from ctx. Returns nil if none is set.
 func GetDocumentLocator(ctx context.Context) DocumentLocator {
+	if ctx == nil {
+		return nil
+	}
 	loc, _ := ctx.Value(documentLocatorKey).(DocumentLocator)
 	return loc
 }
