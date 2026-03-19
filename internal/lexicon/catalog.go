4e9dd19c47c8c5b5f535f7703dcae98ec1572274
Daisuke Maki <lestrrat+github@gmail.com>
2026-03-19 22:26:24 +0900

    extract catalog vocab into lexicon

---
+++ internal/lexicon/catalog.go
@@ -0,0 +1,32 @@
+package lexicon
+
+const (
+	ElemGroup          = "group"
+	ElemPublic         = "public"
+	ElemSystem         = "system"
+	ElemURI            = "uri"
+	ElemRewriteSystem  = "rewriteSystem"
+	ElemRewriteURI     = "rewriteURI"
+	ElemDelegatePublic = "delegatePublic"
+	ElemDelegateSystem = "delegateSystem"
+	ElemDelegateURI    = "delegateURI"
+	ElemNextCatalog    = "nextCatalog"
+)
+
+const (
+	AttrPrefer              = "prefer"
+	AttrPublicID            = "publicId"
+	AttrPublicIDStartString = "publicIdStartString"
+	AttrSystemID            = "systemId"
+	AttrSystemIDStartString = "systemIdStartString"
+	AttrURI                 = "uri"
+	AttrURIStartString      = "uriStartString"
+	AttrRewritePrefix       = "rewritePrefix"
+	AttrCatalog             = "catalog"
+	AttrName                = "name"
+)
+
+const (
+	PreferPublic = "public"
+	PreferSystem = "system"
+)
---
+++ internal/lexicon/xml.go
@@ -0,0 +1,14 @@
+package lexicon
+
+const (
+	PrefixXML   = "xml"
+	PrefixXMLNS = "xmlns"
+
+	AttrBase  = "base"
+	AttrID    = "id"
+	AttrLang  = "lang"
+	AttrSpace = "space"
+
+	SpaceDefault  = "default"
+	SpacePreserve = "preserve"
+)
