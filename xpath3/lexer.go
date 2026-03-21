0f7119f028de74549b806a69211fa01780a650f7
Daisuke Maki <lestrrat+github@gmail.com>
2026-03-22 08:39:33 +0900

    fix xpath3 NCName validation to use XML spec ranges

--- xpath3/lexer.go
+++ xpath3/lexer.go
@@ -554,15 +554,29 @@ func (l *lexer) PrettyTokens() string {
 	return b.String()
 }
 
-// isNCNameStart returns true if r is a valid start of an NCName.
+// isNCNameStart returns true if r is a valid start of an NCName per the XML spec.
+// NCName ::= Name - (Char* ':' Char*), so NameStartChar minus ':'.
+// NameStartChar ::= ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] |
+//
+//	[#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] |
+//	[#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] |
+//	[#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
 func isNCNameStart(r rune) bool {
-	return unicode.IsLetter(r) || r == '_'
+	return (r >= 'A' && r <= 'Z') || (r >= 'a' && r <= 'z') || r == '_' ||
+		(r >= 0xC0 && r <= 0xD6) || (r >= 0xD8 && r <= 0xF6) ||
+		(r >= 0xF8 && r <= 0x2FF) || (r >= 0x370 && r <= 0x37D) ||
+		(r >= 0x37F && r <= 0x1FFF) || (r >= 0x200C && r <= 0x200D) ||
+		(r >= 0x2070 && r <= 0x218F) || (r >= 0x2C00 && r <= 0x2FEF) ||
+		(r >= 0x3001 && r <= 0xD7FF) || (r >= 0xF900 && r <= 0xFDCF) ||
+		(r >= 0xFDF0 && r <= 0xFFFD) || (r >= 0x10000 && r <= 0xEFFFF)
 }
 
-// isNCNameChar returns true if r is a valid NCName continuation character.
+// isNCNameChar returns true if r is a valid NCName continuation character per the XML spec.
+// NameChar ::= NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
 func isNCNameChar(r rune) bool {
-	return unicode.IsLetter(r) || unicode.IsDigit(r) ||
-		r == '.' || r == '-' || r == '_'
+	return isNCNameStart(r) ||
+		(r >= '0' && r <= '9') || r == '-' || r == '.' ||
+		r == 0xB7 || (r >= 0x0300 && r <= 0x036F) || (r >= 0x203F && r <= 0x2040)
 }
 
 // isValidNCName checks whether s is a valid XML NCName (non-empty, starts with
