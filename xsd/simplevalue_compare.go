2e8ace6463632819e8501e27fad75edc770f783c
Daisuke Maki <lestrrat+github@gmail.com>
2026-03-27 12:21:17 +0900

    remove unused compareValues wrapper

--- xsd/simplevalue_compare.go
+++ xsd/simplevalue_compare.go
@@ -2,13 +2,6 @@ package xsd
 
 import "github.com/lestrrat-go/helium/internal/xsd/value"
 
-// compareValues dispatches to type-specific comparison.
-// Returns (cmp, ok) where cmp is -1/0/+1 and ok is false when comparison
-// is undefined (NaN, incomparable durations, parse failures).
-func compareValues(a, b, builtinLocal string) (int, bool) {
-	return value.Compare(a, b, builtinLocal)
-}
-
 // compareDecimal compares two decimal string values using math/big.Rat.
 // Returns -1 if a < b, 0 if a == b, 1 if a > b, or -2 on parse error.
 func compareDecimal(a, b string) int {
