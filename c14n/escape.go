9ca6fd6f6344c12804bd46f2cf08cf231ad18163
Daisuke Maki <49281+lestrrat@users.noreply.github.com>
2026-06-21 20:36:57 +0900

    dedup c14n escape and ns/attr inheritance

--- c14n/escape.go
+++ c14n/escape.go
@@ -15,23 +15,15 @@ var (
 	escCR   = []byte("&#xD;")
 )
 
-// escapeText escapes text node content per C14N rules:
-// & → &amp;  < → &lt;  > → &gt;  \r → &#xD;
-func escapeText(w io.Writer, s []byte) error {
+// escapeRunes walks s as UTF-8 and writes it to w, replacing any rune for which
+// repl returns a non-nil byte slice with that slice. Runs of unreplaced bytes
+// are written verbatim in a single Write.
+func escapeRunes(w io.Writer, s []byte, repl func(rune) []byte) error {
 	last := 0
 	for i := 0; i < len(s); {
 		r, width := utf8.DecodeRune(s[i:])
-		var esc []byte
-		switch r {
-		case '&':
-			esc = escAmp
-		case '<':
-			esc = escLT
-		case '>':
-			esc = escGT
-		case '\r':
-			esc = escCR
-		default:
+		esc := repl(r)
+		if esc == nil {
 			i += width
 			continue
 		}
@@ -50,43 +42,44 @@ func escapeText(w io.Writer, s []byte) error {
 	return nil
 }
 
+// escapeText escapes text node content per C14N rules:
+// & → &amp;  < → &lt;  > → &gt;  \r → &#xD;
+func escapeText(w io.Writer, s []byte) error {
+	return escapeRunes(w, s, func(r rune) []byte {
+		switch r {
+		case '&':
+			return escAmp
+		case '<':
+			return escLT
+		case '>':
+			return escGT
+		case '\r':
+			return escCR
+		}
+		return nil
+	})
+}
+
 // escapeAttrValue escapes attribute values per C14N rules:
 // & → &amp;  < → &lt;  " → &quot;  \t → &#x9;  \n → &#xA;  \r → &#xD;
 func escapeAttrValue(w io.Writer, s []byte) error {
-	last := 0
-	for i := 0; i < len(s); {
-		r, width := utf8.DecodeRune(s[i:])
-		var esc []byte
+	return escapeRunes(w, s, func(r rune) []byte {
 		switch r {
 		case '&':
-			esc = escAmp
+			return escAmp
 		case '<':
-			esc = escLT
+			return escLT
 		case '"':
-			esc = escQuot
+			return escQuot
 		case '\t':
-			esc = escTab
+			return escTab
 		case '\n':
-			esc = escNL
+			return escNL
 		case '\r':
-			esc = escCR
-		default:
-			i += width
-			continue
+			return escCR
 		}
-		if _, err := w.Write(s[last:i]); err != nil {
-			return err
-		}
-		if _, err := w.Write(esc); err != nil {
-			return err
-		}
-		i += width
-		last = i
-	}
-	if _, err := w.Write(s[last:]); err != nil {
-		return err
-	}
-	return nil
+		return nil
+	})
 }
 
 // escapePIOrComment escapes processing instruction or comment content:
