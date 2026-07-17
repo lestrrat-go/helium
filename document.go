3abf10e596d75da166d418c666852b0857db2ec6
Daisuke Maki <49281+lestrrat@users.noreply.github.com>
2026-07-17 13:23:55 +0900

    make SetTreeDoc doc precise: descendants not already owned by doc

--- document.go
+++ document.go
@@ -205,7 +205,8 @@ func (d *Document) AddSibling(_ Node) error {
 	return errors.New("can't add sibling to a document")
 }
 
-// SetTreeDoc sets doc as the owning document of this node and its subtree.
+// SetTreeDoc sets doc as the owning document of this node and of each
+// descendant not already owned by doc.
 func (d *Document) SetTreeDoc(doc *Document) {
 	setTreeDoc(d, doc)
 }
